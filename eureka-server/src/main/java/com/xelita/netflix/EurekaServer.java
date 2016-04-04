d758ee4b8af82a8a83213802ce48591cddecfbe0
xelita <admin@xelita.com>
2016-04-04 15:50:40 +0200

    Initial commit

---
+++ eureka-server/src/main/java/com/xelita/netflix/EurekaServer.java
@@ -0,0 +1,17 @@
+package com.xelita.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication eurekaServer = new SpringApplication(EurekaServer.class);
+        eurekaServer.addListeners(new ApplicationPidFileWriter("eureka-server.pid"));
+        eurekaServer.run(args);
+    }
+}
